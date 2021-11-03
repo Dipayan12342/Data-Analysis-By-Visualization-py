9ff28f2e5b18fe77a0f26738d85b0b858a77e37e
Dipayan12342 <74650758+Dipayan12342@users.noreply.github.com>
2021-11-03 16:27:59 +0530

    Update student.py

--- student.py
+++ student.py
@@ -3,6 +3,6 @@ import csv
 import plotly.express as px 
 
 df = pd.read_csv("data.csv")
-ariana = df.groupby(["student_id", "level"], as_index = False)["attempt"].mean()
-fig = px.scatter(ariana, x="student_id", y="level", size="attempt", color="attempt")
+dipayan = df.groupby(["student_id", "level"], as_index = False)["attempt"].mean()
+fig = px.scatter(dipayan, x="student_id", y="level", size="attempt", color="attempt")
 fig.show()
